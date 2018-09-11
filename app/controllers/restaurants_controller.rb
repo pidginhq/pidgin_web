968737496760c3352f856050d83ff32ea089f858
SeanStreak <seanmainastreak@gmail.com>
2018-09-10 22:18:13 -0400

    Restaurants Page Created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,11 @@
 class RestaurantsController < ApplicationController
 	def index
-		@restaurant_search_results = Restaurant.where('restaurant_name LIKE ?', "%#{params[:restaurant_name]}%")	
+		#Query results will be
+		@restaurant_search_results = Restaurant.where('restaurant_name LIKE ?', "%#{params[:restaurant_query].to_s.capitalize}%");
+	end
+	def show
+		@restaurant = Restaurant.find(params[:id])
+		puts @restaurant
 	end
 	def restaurant_params
   		params.require(:restaurant).permit(:restaurant_name)
