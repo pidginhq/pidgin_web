3c699ac385e753e605c109776757dad0d3898066
SeanStreak <seanmainastreak@gmail.com>
2018-11-04 22:15:39 -0500

    Mailer connection

--- app/controllers/waitlists_controller.rb
+++ app/controllers/waitlists_controller.rb
@@ -5,8 +5,8 @@ class WaitlistsController < ApplicationController
     	if @person.save
 
      		# Sends email to user when user is created.
-      #PidginMailer.signup_email(@person).deliver_now
-			#flash[:message] = 'Mail was successfully created.' 
+        PidginMailer.signup_email(@person).deliver_now
+			  flash[:message] = 'Mail was successfully created.' 
     		else
       			format.html { render :new }
       			format.json { render json: @user.errors, status: :unprocessable_entity }
