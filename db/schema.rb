8d037950f921d998a4f4c28c7d77c773f57f3335
SeanStreak <seanmainastreak@gmail.com>
2018-11-04 22:44:44 -0500

    schema.rb alter

--- db/schema.rb
+++ db/schema.rb
@@ -15,46 +15,6 @@ ActiveRecord::Schema.define(version: 20181103222816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "colleges", force: :cascade do |t|
-    t.string "college_name"
-    t.integer "university_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "dorms", force: :cascade do |t|
-    t.string "dorm_name"
-    t.integer "delivery_zone"
-    t.integer "user_count"
-  end
-
-  create_table "menu_items", force: :cascade do |t|
-    t.integer "menu_id"
-    t.string "menu_item_name"
-    t.string "menu_section"
-    t.float "item_price"
-  end
-
-  create_table "menu_menu_items", force: :cascade do |t|
-    t.integer "menu_id"
-    t.integer "item_id"
-  end
-
-  create_table "menus", force: :cascade do |t|
-    t.string "menu_name"
-    t.integer "restaurant_id"
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.integer "restaurant_id"
-    t.string "restaurant_name"
-    t.string "restaurant_location"
-    t.string "restaurant_food_genre"
-    t.string "price"
-    t.string "profile_image_url"
-    t.integer "menu_id"
-  end
-
   create_table "waitlists", force: :cascade do |t|
     t.text "email"
     t.datetime "created_at", null: false
