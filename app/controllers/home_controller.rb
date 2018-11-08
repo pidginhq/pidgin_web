dcba9ebb4effafd83da23bb6ee327111c329c69f
SeanStreak <seanmainastreak@gmail.com>
2018-11-07 19:38:00 -0500

    About page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+	#Home
 	def index
 	end
+	#About
+	def about
+	end
+	#FAQ
 	def faq
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => "home#index"
   get '/faq' => "home#faq"
+  get '/about' => "home#about"
   resource :waitlist
   get '/waitlist/new' => "home#index"
   get '/waitlist/show' => "waitlists#show"
