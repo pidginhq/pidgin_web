f77ed042e92ede5d67b5b54ab7763253ed23c72f
SeanStreak <seanmainastreak@gmail.com>
2018-11-10 23:15:25 -0500

    Authentication on Mailer correction - Mailer Loaded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "pidgin.hq@gmail.com",
-    :password             => "innovatewing102018!",
+    :user_name            => ENV["gmail_username"],
+    :password             => ENV["gmail_password"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
