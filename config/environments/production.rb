80ccd2d683cddf1112446aa681f032fab6d09c39
SeanStreak <seanmainastreak@gmail.com>
2018-11-10 22:59:54 -0500

    production.rb authentication change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => "pidgin.hq@gmail.com",
+    :password             => "innovatewing62018!",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
