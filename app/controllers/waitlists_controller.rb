23da601ed04ecaaca9893780c38f0c11f8b8d650
SeanStreak <seanmainastreak@gmail.com>
2018-11-10 22:31:08 -0500

    Mailer removal of .save in waitlists_new

--- app/controllers/waitlists_controller.rb
+++ app/controllers/waitlists_controller.rb
@@ -3,15 +3,7 @@ class WaitlistsController < ApplicationController
 	def new
 		@person = Waitlist.new
 		@person.email = params[:email]
-    	if @person.save
-
-     		# Sends email to user when user is created.
-        PidginMailer.signup_email(@person).deliver_now
-			  flash[:message] = 'Mail was successfully created.' 
-    		else
-      			format.html { render :new }
-      			format.json { render json: @user.errors, status: :unprocessable_entity }
-    		end 	
+    @person.save 	
     end
     #Show
     def show
